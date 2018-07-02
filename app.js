748e81a73057e54a682afa4cc4ee306eeb5e6537
lileilei <670029024@qq.com>
2018-07-02 16:18:48 +0800

    开工 package.json

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import routes from './routes';
 const app=express();
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+    res.header("X-Powered-By", '3.2.1')
+    if (req.method == 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
 });
+routes(app);
 let server = app.listen(3000, ()=> {
     let host = server.address().address;
     let port = server.address().port;
