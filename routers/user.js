aa7ae687e3d02af6ea3f0b09807222b4794ec5d6
lileilei <670029024@qq.com>
2020-02-01 17:03:10 +0800

    12332465465

--- routers/user.js
+++ routers/user.js
@@ -52,4 +52,40 @@ router.post('/register',async(ctx) =>{
         }
     }
 })
+// token鉴权
+const jwt =require('jsonwebtoken');
+const jwtAuth=require('koa-jwt');
+const secret='re';
+router.post('/login-token',async(ctx) =>{
+    const {body}=ctx.request;
+    body.password=md5(md5(body.password),body.username);
+    const userInfo=await User.findOne({username:body.username});
+    if(userInfo&&body.password===userInfo.password){
+        const{password,...userInfoData}=userInfo;
+        ctx.session.userInfo=userInfoData;
+        ctx.body={
+            success:true,
+            message:'登陆成功！',
+            data:{
+                userInfoData,
+                token:jwt.sign({
+                    data:userInfo,
+                    exp:Math.floor(Date.now()/1000)+60*60,
+                }, secret)
+            }
+        }
+    }else{
+        ctx.body={
+            success:false,
+            message:'用户名或密码错误'
+        }  
+    }
+})
+router.get('/userInfo-token',jwtAuth({secret}),async(ctx) =>{
+    ctx.body={
+        success:true,
+        data:ctx.state.user.data,
+        message:'查询成功！'
+    };
+})
 module.exports=router;
